7699fb7ee44ce777f879ab22d6556b430407d58c
Oluwatosin Emmanuel Amokeodo <oluwatosin.amokeodo@gmail.com>
2023-04-26 14:02:01 +0100

    Update AdminInterface.php

--- app/Contracts/AdminInterface.php
+++ app/Contracts/AdminInterface.php
@@ -3,10 +3,4 @@
 namespace App\Contracts;
 
 interface AdminInterface
-{
-    public function editUser($request);
-    public function deleteUser($request);
-    public function allUsers($request);
-    public function getUser($request);
-
-}
+{}
