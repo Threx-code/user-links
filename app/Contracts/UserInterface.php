27e8d7b54d842166b92252132fc40db6c0c582a5
Oluwatosin Emmanuel Amokeodo <oluwatosin.amokeodo@gmail.com>
2023-04-26 14:02:33 +0100

    Update UserInterface.php

--- app/Contracts/UserInterface.php
+++ app/Contracts/UserInterface.php
@@ -3,11 +3,4 @@
 namespace App\Contracts;
 
 interface UserInterface
-{
-    public function createUser($request);
-    public function generateNewLink($request);
-    public function deactivateLink($request);
-    public function linkIsValid($request);
-    public function feelingLucky($request);
-    public function history($request);
-}
+{}
